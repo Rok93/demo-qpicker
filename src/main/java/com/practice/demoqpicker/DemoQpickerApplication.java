242f7bf374fcb31eabc362740405702bdc118bc5
김경록 <goodboy3421@gmail.com>
2020-11-12 21:52:54 +0900

    Initial commit

---
+++ src/main/java/com/practice/demoqpicker/DemoQpickerApplication.java
@@ -0,0 +1,13 @@
+package com.practice.demoqpicker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoQpickerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoQpickerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/practice/demoqpicker/DemoQpickerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.demoqpicker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoQpickerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
